88a26e05f510a3c2e53ee6928c345497541765c8
Cabral2104 <valdezemilio50@gmail.com>
2025-02-28 11:27:48 -0600

    v1.1.1

--- Asistente_Voz.py
+++ Asistente_Voz.py
@@ -28,8 +28,7 @@ def talk():
         print("Hubo un error con el reconocimiento de voz.")
         return ""
 
-def search_store(store_name, url):
-    engine.say(f'Qué quieres buscar en {store_name}?')
+def search_store(service_name, url):
     engine.runAndWait()
     query = talk()
     
@@ -37,24 +36,26 @@ def search_store(store_name, url):
         webbrowser.open(url.format(query=query))
 
 #Lista de tiendas disponibles
-stores = {
+services = {
     "amazon": "https://www.amazon.es/s?k={query}",
     "mercado libre": "https://www.mercadolibre.com.mx/jm/search?as_word={query}",
     "ebay": "https://www.ebay.com/sch/i.html?_nkw={query}",
     "aliexpress": "https://www.aliexpress.com/wholesale?SearchText={query}",
     "walmart": "https://www.walmart.com.mx/search?q={query}",
-    "temu": "https://www.temu.com/search?q={query}"
+    "temu": "https://www.temu.com/search?q={query}",
+    "youtube": "https://www.youtube.com/results?search_query={query}"
+
 }
 
 #Capturar el texto hablado
 text = talk()
 
 #Buscar coincidencias con las tiendas disponibles
-match = difflib.get_close_matches(text, stores.keys(), n=1, cutoff=0.6)
+match = difflib.get_close_matches(text, services.keys(), n=1, cutoff=0.6)
 
 if match:
-    store_name = match[0]
-    search_store(store_name, stores[store_name])
+    service_name = match[0]
+    search_store(service_name, services[service_name])
 else:
     print(f"No se reconoció la tienda: {text}")
     engine.say("No reconocí la tienda, intenta de nuevo.")
